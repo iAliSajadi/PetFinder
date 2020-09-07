f27dad200b49a932463eb89268e6abe4a7230a96
ali Sajadi <chargoonmobileintern2@outlook.com>
2020-09-07 19:07:08 +0430

    Modify login view controller for dismissing keyboard and login with enter key

--- PetFinder/Controller/LoginViewController.swift
+++ PetFinder/Controller/LoginViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var APIKeyTextField: UITextField!
     @IBOutlet var secretTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
     
     let store = Store()
     let userAlert = UserAlert()
@@ -19,7 +20,12 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        // Specify delegates
+        APIKeyTextField.delegate = self
+        secretTextField.delegate = self
+        
+        // Calling required method
+        createTapGestureRecognizer()
     }
     
     @IBAction func login(_ sender: UIButton) {
@@ -46,6 +52,35 @@ class LoginViewController: UIViewController {
             userAlert.showInfoAlert(title: "Error", message: "The API Key Cann't be empty", view: self, action: {()})
         } else if let secret = secret, secret.isEmpty {
             userAlert.showInfoAlert(title: "Error", message: "The secret Cann't be empty", view: self, action: {()})
+        }
+    }
+    
+    //MARK: - Dismissing KeyBoard
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        // Login with enter key
+        login(loginButton)
+        
+        return true
+    }
+    
+    private func createTapGestureRecognizer() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        view.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    //MARK: - trimming whitespaces in API Key TextField
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.tag == 1 {
+            APIKeyTextField.text = APIKeyTextField.text?.trimmingCharacters(in: .whitespaces)
         }
     }
 }
